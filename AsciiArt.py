ab16de2ff1c82a23902ff787a4dd6ebca291af6e
Ttst49 <thibautstachnick@gmail.com>
2023-01-24 09:56:13 +0100

    commit stream

--- AsciiArt.py
+++ AsciiArt.py
@@ -4,8 +4,7 @@ import math
 from PIL import Image
 
 gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\^`'."
-
-gscale2 = "@%#*+=-:."
+gscale2 = "@%#*+=-:.M"
 
 
 def averageL(img):
@@ -15,3 +14,77 @@ def averageL(img):
     w, h = image.shape
 
     return banane.average(image.reshape(w*h))
+
+
+def convertIntoAscii(imageFile, colonnes, echelle, niveauxSup):
+    global gscale1, gscale2
+
+    image = Image.open(imageFile).convert('L')
+
+    W, H = image.size[0], image.size[1]
+
+    w = W / colonnes
+    h = w / echelle
+    rows = int(H / h)
+
+    aimg = []
+
+    for i in range(rows):
+        y1 = int(i * h)
+        y2 = int((i + 1) * h)
+
+        if i == rows - 1:
+            y2 = H
+
+        aimg.append("")
+
+        for j in range(colonnes):
+
+            x1 = int(j * w)
+            x2 = int((j + 1) * w)
+
+            if j == colonnes - 1:
+                x2 = W
+
+            img = image.crop((x1, y1, x2, y2))
+
+            avg = int(averageL(img))
+
+            if niveauxSup:
+                gsval = gscale1[int((avg * 68) / 255)]
+            else:
+                gsval = gscale2[int((avg * 8) / 255)]
+
+            aimg[i] += gsval
+    return aimg
+
+
+def main():
+    descStr = "make ascii art"
+    parser = argparse.ArgumentParser(description=descStr)
+
+    parser.add_argument('--file', dest='imgFile', required=False)
+    parser.add_argument('--echelle', dest='echelle', required=False)
+    parser.add_argument('--colonnes', dest='colonnes', required=False)
+    parser.add_argument('--niveauxSup', dest='niveauxSups', action='store_true')
+
+    # parse args
+    args = parser.parse_args()
+
+    imgFile = args.imgFile
+
+    echelle = 0.40
+    if args.echelle:
+        echelle = float(args.echelle)
+
+    colonnes = 250
+    if args.colonnes:
+        colonnes = int(args.colonnes)
+
+    aimg = convertIntoAscii("images/image3.jpg", colonnes, echelle, args.niveauxSups)
+
+    for row in aimg:
+        print(row + '\n')
+
+
+main()
---
+++ asciiStream.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from numba import jit
+
+@jit(nopython=True)
+def to_ascii_art(frame, images, box_height=12, box_width=16):
+    height, width = frame.shape
+    for i in range(0, height, box_height):
+        for j in range(0, width, box_width):
+            roi = frame[i:i + box_height, j:j + box_width]
+            best_match = np.inf
+            best_match_index = 0
+            for k in range(1, images.shape[0]):
+                total_sum = np.sum(np.absolute(np.subtract(roi, images[k])))
+                if total_sum < best_match:
+                    best_match = total_sum
+                    best_match_index = k
+            roi[:,:] = images[best_match_index]
+    return frame
+
+def generate_ascii_letters():
+    images = []
+    #letters = "# $%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    letters = " \ \ '(),-./:;[]_`{|}~ "
+    for letter in letters:
+        img = np.zeros((12, 16), np.uint8)
+        img = cv2.putText(img, letter, (0, 11), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
+        images.append(img)
+    return np.stack(images)
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+images = generate_ascii_letters()
+while True:
+
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    gb = cv2.blur(frame, (5, 5), 0)
+    can = cv2.Canny(gb, 127, 31)
+    ascii_art = to_ascii_art(can, images)
+    cv2.imshow('ASCII ART', ascii_art)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
